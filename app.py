87d5396988f88760904253c4e9a292d82162499a
Asad777 <muhammadasad7.7.2007@gmail.com>
2024-12-15 01:32:57 +0500

    adding task done

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.title}"
+
+
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+    if request.method=='POST':
+        title_a =request.form.get("title")
+        description_a =request.form.get("description")
+        todo = Todo(title=title_a, description=description_a)
+    # todo = Todo(title = "1st Title", description = "1st Description")
+        db.session.add(todo)
+        db.session.commit()
+    all_todo = Todo.query.all()
+    return render_template("index.html", all_todo = all_todo)
+
+if __name__==("__main__"):
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host="0.0.0.0", port="8000")
